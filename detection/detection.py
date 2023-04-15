57570cf8a7e5d2c807b57eeb78130ba2fc6fc33c
HXY120369 <1367041395@qq.com>
2023-04-15 15:04:01 +0800

    检查一下sada

--- detection/detection.py
+++ detection/detection.py
@@ -23,7 +23,7 @@ from datetime import timedelta
 from AC_automata import ac_automation
 import numpy as np
 from ocr_baidu import *
-#78948978999
+#dsasd
 
 
 # Add your Varial Here: (Keep This Line of comments)
